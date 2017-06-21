865a0acd3d263da90d49d2b02a24eab4877652c9
sean darsie <sdarsie@e1z2r5p26.42.us.org>
2017-06-21 15:17:34 -0700

    sadf

--- ft_printf.c
+++ ft_printf.c
@@ -1,14 +1,21 @@
 #include "unistd.h"
 #include "stdarg.h"
 
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
 int ft_printf(char *fmt, ...)
 {
 	int i = 0;
 	va_list ap;
 
 	while(fmt[i])
-		write(1, fmt[i++], 1);
+		ft_putchar(fmt[i++]);
 	va_start(ap, fmt);
 	va_end(ap);
+	return (0);
 }
+
 
